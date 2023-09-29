9fb30b093d47d893ac4e125dbcc24a777833830b
Vrits <saidhalim687@gmail.com>
2023-09-30 07:05:40 +0800

    Set

---
+++ bin/set.dart
@@ -0,0 +1,14 @@
+void main () {
+  Set<int> angka = {1,3,2};
+
+  print(angka);
+  print(angka.length);
+  angka.add(4);
+  print(angka);
+  angka.remove(2);
+  print(angka);
+
+  var name = {'Tri', 'Telkomsel'};
+  print(name);
+  
+}

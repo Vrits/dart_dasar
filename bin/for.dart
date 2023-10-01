31b5e450b02256632b133b08bda29eafa4b4d663
Vrits <saidhalim687@gmail.com>
2023-10-02 07:36:56 +0800

    For

---
+++ bin/for.dart
@@ -0,0 +1,17 @@
+void main() {
+  // int count = 0;
+
+  // for (; count < 10; ){
+  //   print('Perulangan ke-${count+1}');
+  //   count++;
+  // }
+
+  // for (int count = 0; count < 10; ){
+  //   print('Perulangan ke-${count+1}');
+  //   count++;
+  // }
+
+  for (int count = 0; count < 10; count++) {
+    print('Perulangan ke-${count + 1}');
+  }
+}

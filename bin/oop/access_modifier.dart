8c37500188d17acc3d9421e444d807de457f586d
Vrits <saidhalim687@gmail.com>
2023-10-18 06:31:19 +0800

    Access Modifier

---
+++ bin/oop/access_modifier.dart
@@ -0,0 +1,8 @@
+import 'data/product.dart';
+
+void main() {
+  Product e = Product();
+  e.id = '1';
+  e.name = 'Laptop';
+  // e._quantity = 0;
+}
---
+++ bin/oop/data/product.dart
@@ -0,0 +1,7 @@
+class Product {
+  String? id;
+  String? name;
+  int? _quantity;
+
+  int? getQuantity() => _quantity;
+}

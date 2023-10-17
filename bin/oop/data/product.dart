3dc2e7a90798c2f5a1cba1e040cebe34726a895c
Vrits <saidhalim687@gmail.com>
2023-10-18 07:16:00 +0800

    To String

--- bin/oop/data/product.dart
+++ bin/oop/data/product.dart
@@ -4,4 +4,6 @@ class Product {
   int _quantity = 10;
 
   int? getQuantity() => _quantity;
+
+  String toString() => 'Product{id: $id, name: $name,quantity: $_quantity}';
 }
---
+++ bin/oop/to_string.dart
@@ -0,0 +1,10 @@
+import 'data/product.dart';
+import 'data/category.dart';
+void main() {
+  Product e = Product();
+  Category f = Category('1', 'Idk');
+  print(e.toString());
+  print(e);
+  print(f);
+  
+}

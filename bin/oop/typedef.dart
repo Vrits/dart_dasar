2e8c66781b5ee0b0065ea29961587e4ff0ddc434
Vrits <saidhalim687@gmail.com>
2023-10-18 11:24:40 +0800

    Typedef Function

--- bin/oop/typedef.dart
+++ bin/oop/typedef.dart
@@ -1,3 +1,4 @@
+typedef Hitung = int Function(int,int);
 class Sum {
   int first;
   int second;
@@ -5,12 +6,21 @@ class Sum {
   Sum(this.first, this.second);
 
   int call() => first+ second;
+  void printHitung (int a, int b, Hitung c) {
+    print(c(a,b));
+
+  }
 }
+
 typedef Total = Sum;
+
+
 void main() {
   Total s = Total(10, 20);
 
   var total = s();
   print(total);
+  
+  s.printHitung(3, 2, (p0, p1) => p0 * p1);
 
 }

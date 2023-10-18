a380ad8f1ba6cd2594a20314cca268fcd3acb636
Vrits <saidhalim687@gmail.com>
2023-10-18 12:58:23 +0800

    Finally

---
+++ bin/oop/finally.dart
@@ -0,0 +1,29 @@
+class Validation {
+  static void validate(String username, String password) {
+    if (username == "") {
+      throw Exception("Username is blank");
+    } else if (password == "") {
+      throw Exception('Password is Blank');
+    } else if (password != "1234" || username != "said") {
+      throw Exception('Login Failed.');
+    }
+  }
+}
+
+class ValidationException implements Exception {
+  String message;
+
+  ValidationException(this.message);
+}
+
+void main() {
+  try {
+    Validation.validate('said', '12345');
+  } on ValidationException catch (e) {
+    print('Validation Error, ${e.message}');
+  } on Exception catch (e) {
+    print('Validation Error, ${e.toString()}');
+  } finally {
+    print('Done');
+  }
+}

51b8891e58f2737a4c1e33005b7e49e90503a98f
Vrits <saidhalim687@gmail.com>
2023-11-06 21:32:15 +0800

    Hash Map

---
+++ bin/collection/hash_map.dart
@@ -0,0 +1,12 @@
+import 'dart:collection';
+
+void main() {
+  final scores = HashMap<String, int>();
+
+  scores['Pace'] = 100;
+  scores['Asep'] = 100;
+  scores['Emanuel'] = 100;
+  scores['Emanuel'] = 100;
+
+  print(scores);
+}
